212be6663bc4bafa3c6dc6e78a0245a18dd3f32e
ederdon <ederpalacin@gmail.com>
2022-03-14 17:25:15 +0000

    Ej23

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -1,6 +1,28 @@
 let parking_state = [
-  [1,1,1],
-  [0,0,0],
-  [1,1,2]
+  [1, 1, 1],
+  [0, 0, 0],
+  [1, 1, 2]
 ]
-console.log(getParkingLotState(parking_state))
+
+const getParkingLotState = (parking_state) => {
+
+  let state = {
+    availableSlots: 0,
+    occupiedSlots: 0,
+    totalSlots: 0
+  }
+
+  for (let rows in parking_state) {
+    for (let statenumber of parking_state[rows]) {
+      if (statenumber != 0) {
+        state.totalSlots++;
+        if (statenumber == 1) state.occupiedSlots++;
+        else if (statenumber == 2) state.availableSlots++;
+      }
+    }
+  }
+
+  return state;
+}
+
+console.log(getParkingLotState(parking_state));
